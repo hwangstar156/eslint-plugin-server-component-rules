d16b65b0900dd756bebfdf5ea4f437d81c8acdf8
hwangjungmin <hwangstar156@gmail.com>
2023-06-11 17:55:11 +0900

    feat: add no-use-react-hook rules

---
+++ plugins/index.js
@@ -0,0 +1,15 @@
+const { fileNameRules } = require("./rules/file-name");
+const { noImportUseClientRules } = require("./rules/no-import-use-client");
+const { noUserBrowserApi } = require("./rules/no-use-browser-api");
+const { noUseEventHandler } = require("./rules/no-use-event-handler");
+const { noUseReactHook } = require("./rules/no-use-react-hook");
+
+module.exports = {
+  rules: {
+    'file-name': {...fileNameRules},
+    'no-import-use-client': {...noImportUseClientRules},
+    'no-use-react-hook': {...noUserBrowserApi},
+    'no-use-browser-api': {...noUseEventHandler},
+    'no-use-event-handler': {...noUseReactHook},
+  },
+};
