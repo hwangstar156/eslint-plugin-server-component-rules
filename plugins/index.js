61724bfc23cf1974608f39095968aa48fabaff0f
hwangstar156 <hwangstar156@gmail.com>
2023-07-19 11:45:52 +0900

    fix: import하지 못하는 문제 수정

--- plugins/index.js
+++ plugins/index.js
@@ -1,8 +1,8 @@
-const { fileNameRules } = require("./rules/file-name");
-const { noImportUseClientRules } = require("./rules/no-import-use-client");
-const { noUserBrowserApi } = require("./rules/no-use-browser-api");
-const { noUseEventHandler } = require("./rules/no-use-event-handler");
-const { noUseReactHook } = require("./rules/no-use-react-hook");
+const  fileNameRules  = require("./rules/file-name");
+const  noImportUseClientRules  = require("./rules/no-import-use-client");
+const  noUserBrowserApi  = require("./rules/no-use-browser-api");
+const  noUseEventHandler  = require("./rules/no-use-event-handler");
+const  noUseReactHook  = require("./rules/no-use-react-hook");
 
 module.exports = {
   rules: {
